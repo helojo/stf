b312e813f3adf1666a72e5806fa826ba754fcaa4
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-02-13 02:15:38 +0100

    email validity test stub added

--- test/util/oauth2_domain.js
+++ test/util/oauth2_domain.js
@@ -1,10 +1,22 @@
 var chai = require('chai')
 var expect = chai.expect
 
-describe('oauth2', function() {
-  var oauth2 = require('../../lib/units/auth/oauth2')({})
-  it('should validate any email if restriction not specified', function() {
-    expect(oauth2.isEmailAllowed('test@test.test')).to.be.true
+describe('oauth2 domain', function() {
+  describe('email restriction not specified', function() {
+    var oauth2 = require('../../lib/units/auth/oauth2')()
+    it('should validate any email', function () {
+      expect(oauth2.isEmailAllowed('test@test.test')).to.be.true
+      expect(oauth2.isEmailAllowed('test@test.invalid')).to.be.true
+    })
+  })
+
+  describe('email restriction specified', function() {
+    var oauth2 = require('../../lib/units/auth/oauth2')({
+      domain: 'company.test'
+    })
+    it('should not validate email outside allowed domain', function () {
+      expect(oauth2.isEmailAllowed('test@test.test')).to.be.true
+    })
   })
 
 })
